0c9ac193c7eaed054f1632eff62866d9dbf2b9ab
mmmm-lobster <letttuceman@gmail.com>
2024-06-13 21:24:21 -0700

    Updated word_game.py
    Moved all prints to the end of the game. 
    There is now a loss message.
    No more "game ended" message whenever the game ends

--- word_game.py
+++ word_game.py
@@ -39,25 +39,42 @@ word = "chair" #set the word here, scroll down, hit play,
 
 
 
-game_ended = False
+
 print("You have 5 tries to guess the 5 letter word.")
 print(" Every guess tells you how many letters your guess  shares with the word")
 def checkguess(guess):
+  validguess = True
+  correctnums = 0
+  GameWon = False
   if len(guess) ==5:
+    
     correctnums = 0
     for x in guess:
      if x in word:
        correctnums = correctnums + 1
-    print(correctnums)
+   
     if guess == word:
-      print("You did it!!!")
-      global game_ended
-      game_ended = True
+      GameWon = True
+    
   else:
-    print("Guess needs to be 5 letters")
-for _ in range(1,20):
-  if not game_ended:
-    guess = input("Guess a 5 letter word: ")
-    checkguess(guess)
+    validguess = False
+  return(validguess,correctnums,GameWon)
 
-print("Game Ended")
+for i in range(1,21):
+ 
+  guess = input("Guess a 5 letter word: ")
+  guesschecked = checkguess(guess)
+  if not guesschecked[2]:
+    if guesschecked[0]:
+      print(f"Your word shares {guesschecked[1]} letter(s) with the word")
+      print(f"You have used {i} of your 20 guesses")
+    else:
+      print("Word must be 5 letters long")
+  else:
+    print("You won! :D")
+    break
+  if i == 20:
+    print(f"You lost. The word was '{word}' ):")
+    
+
+# print("Game Ended")
